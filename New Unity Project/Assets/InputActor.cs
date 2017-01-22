a89a6bf82e349b8c31dabf52137945075dd19337
shanxuan <781244184@qq.com>
2017-01-22 18:35:54 +0800

    unity project

---
+++ New Unity Project/Assets/InputActor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InputActor : MonoBehaviour {
+
+
+    private InputField inputFiled;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("pass");
+	}
+
+    public void OnSubmit(string text)
+    {
+        Debug.Log("input text : " + text);
+    }
+
+    public void OnTestClick()
+    {
+        Debug.Log("click a button");
+    }
+
+}
